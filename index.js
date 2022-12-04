cc6ed663d8fbac8ae2e32a4f5fd4f4b75a9b4284
Helena Tenev <htenev14@gmail.com>
2022-12-03 21:46:02 -0500

    commit tree ef346e66c2eb343e6e43cfce1f41ef3e77fbd6d5

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+
+
+function superbowlWin(array) {
+    let resultt = array.find(record => {
+        if (record["result"] === "W") {
+            return record;
+        };
+    });
+    if (resultt != undefined) {
+        return resultt.year
+    };
+    // return undefined
+}
+
+
+
+// const ages = [3, 10, 18, 20];
+
+// function checkAge(age) {
+//   return age > 18;
+// }
+
+// function myFunction() {
+//   document.getElementById("demo").innerHTML = ages.find(checkAge);
+// }
